2a2010b95d7e24da435b08fadb84846b1cfd2ef2
jb <relic7@aol.com>
2014-04-28 01:39:36 -0400

    x

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -7,7 +7,6 @@ var imageListData = [];
 
 // DOM Ready =============================================================
 $(document).ready(function() {
-
     // Populate the image table on initial page load
     populateTable();
 
@@ -17,20 +16,19 @@ $(document).ready(function() {
 // Fill table with data
 function populateTable() {
     // Empty content string
+    // Then jQuery AJAX call for JSON
     var tableContent = '';
-    // jQuery AJAX call for JSON
     $.getJSON( '/images/imagelist', function( data ) {
-        // Stick our user data array into a userlist variable in the global object
+        // Stick our image data array into a imagelist variable in the global object
         imageListData = data;
         // For each item in our JSON, add a table row and cells to the content string
         $.each(data, function(){
             tableContent += '<tr>';
-            tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.colorstyle + '" title="Show Details">' + this.colorstyle + '</td>';
+            tableContent += '<td><a href="#" class="linkshowimage" rel="' + this.colorstyle + '" title="Show Details">' + this.colorstyle + '</td>';
             tableContent += '<td>' + this.file_path + '</td>';
-            tableContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
+            tableContent += '<td><a href="#" class="linkdeleteimage" rel="' + this._id + '">delete</a></td>';
             tableContent += '</tr>';
         });
-
         // Inject the whole content string into our existing HTML table
         $('#imageList table tbody').html(tableContent);
     });
@@ -39,23 +37,17 @@ function populateTable() {
 
 // Show image Info
 function showImageInfo(event) {
-
-    // Prevent Link from Firing
+    // First Prevent Link from Firing
     event.preventDefault();
-
     // Retrieve imagename from link rel attribute
-    var thisImageName = $(this).attr('rel');
-
     // Get Index of object based on id value
-    var arrayPosition = imageListData.map(function(arrayItem) { return arrayItem.imagename; }).indexOf(thisImageName);
-
     // Get our image Object
+    var thisImageName = $(this).attr('rel');
+    var arrayPosition = imageListData.map(function(arrayItem) { return arrayItem.imagename; }).indexOf(thisImageName);
     var thisImageObject = imageListData[arrayPosition];
-
     //Populate Info Box
     $('#imageInfoColorstyle').text(thisImageObject.colorstyle);
     $('#imageInfoAlt').text(thisImageObject.alt);
     $('#imageInfoShotNumber').text(thisImageObject.shotnumber);
     $('#imageInfoFilepath').text(thisImageObject.filepath);
-
 };
